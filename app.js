9c12a4d7ed7c04119c478ea661297e69758de1c2
Liam Imray <liamimray@Liams-MacBook-Pro.local>
2022-07-06 21:38:09 +0100

    added dynamic pages using pug, improved styling on add product page

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 5000
 const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
+app.set('view engine', 'pug')
+
 app.use(bodyParser.urlencoded({
     extended: false
 }));
