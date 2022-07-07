6c7c06b388b4a1692424eb2648d698fd8411d6d1
Liam Imray <liamimray@Liams-MacBook-Pro.local>
2022-07-07 22:34:52 +0100

    added ejs files to views folder

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
-app.set('view engine', 'pug')
+app.set('view engine', 'ejs')
 
 app.use(bodyParser.urlencoded({
     extended: false
